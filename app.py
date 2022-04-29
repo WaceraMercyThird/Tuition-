6f17f0a581babc3d41858096a577422d36cb2b1b
WaceraMercyThird <waceraaamercy@gmail.com>
2022-04-29 18:46:50 +0300

    loops

---
+++ app.py
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+tuition =8000
+
+print("Year \tTuition")
+
+
+for x in range(1, 6):
+    tuition += tuition* 5
+    print(x, "\t\t$", format(tuition, '.2f'))
+
+
+salary =200000
+
+print("Year \tSalary")
+
+
+for x in range(1, 11):
+    salary += salary *0.03
+    print(x, "\t\t$", format(tuition, '.2f'))
