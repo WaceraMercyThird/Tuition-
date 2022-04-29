e4f066246121bd8a425c9a025569d8416bbf5ac3
WaceraMercyThird <waceraaamercy@gmail.com>
2022-04-29 19:00:11 +0300

    loops

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ print("Year \tTuition")
 
 
 for x in range(1, 6):
-    tuition += tuition* 5
+    tuition += tuition* 0.03
     print(x, "\t\t$", format(tuition, '.2f'))
 
 
@@ -25,5 +25,5 @@ print("Year \tSalary")
 
 
 for x in range(1, 11):
-    salary += salary *0.03
+    salary += salary * 0.03
     print(x, "\t\t$", format(tuition, '.2f'))
